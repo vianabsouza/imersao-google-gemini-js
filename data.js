1a860acf21e04cc166a45a274fa622d21daec390
Barbara Viana <vianabsouza@gmail.com>
2024-09-06 01:29:49 -0300

    feat: add array of objects with tech women information

---
+++ data.js
@@ -0,0 +1,52 @@
+let data = [
+  {
+    title: "Ada Lovelace",
+    description: "Considerada a primeira programadora do mundo, Ada Lovelace trabalhou com Charles Babbage no desenvolvimento da Máquina Analítica, antecessora dos computadores modernos.",
+    link: "https://en.wikipedia.org/wiki/Ada_Lovelace"
+  },
+  {
+    title: "Grace Hopper",
+    description: "Pioneira na área da programação, Grace Hopper desenvolveu o primeiro compilador e popularizou o termo 'bug' na computação.",
+    link: "https://en.wikipedia.org/wiki/Grace_Hopper"
+  },
+  {
+    title: "Radia Perlman",
+    description: "Engenheira de software, Radia Perlman é conhecida por inventar o protocolo Spanning Tree Protocol (STP), fundamental para a comunicação em redes.",
+    link: "https://en.wikipedia.org/wiki/Radia_Perlman"
+  },
+  {
+    title: "Katherine Johnson",
+    description: "Matemática americana que realizou cálculos complexos para as missões espaciais da NASA, incluindo o programa Apollo.",
+    link: "https://en.wikipedia.org/wiki/Katherine_Johnson"
+  },
+  {
+    title: "Margaret Hamilton",
+    description: "Cientista da computação que desenvolveu o software de bordo para o programa espacial Apollo.",
+    link: "https://en.wikipedia.org/wiki/Margaret_Hamilton_(software_engineer)"
+  },
+  {
+    title: "Reshma Saujani",
+    description: "Advogada e fundadora da Girls Who Code, uma organização sem fins lucrativos dedicada a fechar a lacuna de gênero na tecnologia.",
+    link: "https://en.wikipedia.org/wiki/Reshma_Saujani"
+  },
+  {
+    title: "Timnit Gebru",
+    description: "Cientista da computação e ativista, conhecida por seu trabalho em ética em inteligência artificial.",
+    link: "https://en.wikipedia.org/wiki/Timnit_Gebru"
+  },
+  {
+    title: "Safiya Umoja Noble",
+    description: "Professora associada e diretora do Algorithmic Justice Center, que pesquisa os impactos sociais da inteligência artificial.",
+    link: "https://en.wikipedia.org/wiki/Safiya_Umoja_Noble"
+  },
+  {
+    title: "Latanya Sweeney",
+    description: "Professora de Harvard e fundadora do Data Privacy Lab, que pesquisa privacidade de dados e algoritmos.",
+    link: "https://en.wikipedia.org/wiki/Latanya_Sweeney"
+  },
+  {
+    title: "Barbara Liskov",
+    description: "Cientista da computação, ganhadora do Prêmio Turing, conhecida por suas contribuições para a programação orientada a objetos.",
+    link: "https://en.wikipedia.org/wiki/Barbara_Liskov"
+  }
+];
