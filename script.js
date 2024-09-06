5be41626ba0f3f0c48d5764ac5ffa6c39e80fb4f
Barbara Viana <vianabsouza@gmail.com>
2024-09-06 02:00:19 -0300

    feat: move js logic into button function

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-let section = document.getElementById("resultados-pesquisa");
+let search = () => {
+  let section = document.getElementById("resultados-pesquisa");
+  let results = "";
 
-let results = "";
+  for(let item of data) {
+    results += `
+      <div class="item-resultado">
+        <h2>
+          ${item.title}
+        </h2>
+        <p class="descricao-meta">
+          ${item.description}
+        </p>
+        <a href="${item.link}" target="_blank">
+          Mais informações
+        </a>
+      </div>
+    `
+  }
 
-for(let item of data) {
-  results += `
-    <div class="item-resultado">
-      <h2>
-        ${item.title}
-      </h2>
-      <p class="descricao-meta">
-        ${item.description}
-      </p>
-      <a href="${item.link}" target="_blank">
-        Mais informações
-      </a>
-    </div>
-  `
+  section.innerHTML = results;
 }
-
-section.innerHTML = results;
